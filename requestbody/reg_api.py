cd516c2bc19ab99e69bb97669713d85569137a9d
yonesuke0716 <yone2006521022@yahoo.co.jp>
2023-07-18 22:35:54 +0900

    20230718更新

--- requestbody/reg_api.py
+++ requestbody/reg_api.py
@@ -1,30 +1,34 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+
 import pandas as pd
-import numpy as np
+
+# import numpy as np
 
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+
+# from sklearn.metrics import mean_squared_error
 
 
 class ModelParam(BaseModel):
     random_state: int
     test_size: float
     is_shuffle: bool
+    test_number: int
 
 
 app = FastAPI()
 
 
 @app.post("/")
-def eval_california_price_rmse(params: ModelParam):
+def calc_california_price(params: ModelParam):
     california_housing = fetch_california_housing()
     X = pd.DataFrame(california_housing.data, columns=california_housing.feature_names)
     y = pd.DataFrame(california_housing.target)
     # 訓練データとテストデータに分割する
-    print(params.random_state, params.test_size, params.is_shuffle)
+    print(params.random_state, params.test_size, params.is_shuffle, params.test_size)
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, random_state=params.random_state, test_size=params.test_size, shuffle=params.is_shuffle
     )
@@ -35,13 +39,18 @@ def eval_california_price_rmse(params: ModelParam):
     reg.fit(X_train, y_train)
 
     # 学習データに対するmodelの性能評価
-    model_train_pred = reg.predict(X_train)
-    model_train_mse = mean_squared_error(y_train, model_train_pred)
-    model_train_rmse = np.sqrt(model_train_mse)
+    # model_train_pred = reg.predict(X_train)
+    # model_train_mse = mean_squared_error(y_train, model_train_pred)
+    # model_train_rmse = np.sqrt(model_train_mse)
 
     # 訓練データに対するmodelの性能評価
     model_test_pred = reg.predict(X_test)
-    model_test_mse = mean_squared_error(y_test, model_test_pred)
-    model_test_rmse = np.sqrt(model_test_mse)
+    # model_test_mse = mean_squared_error(y_test, model_test_pred)
+    # model_test_rmse = np.sqrt(model_test_mse)
 
-    return {"train_RMSE": model_train_rmse, "test_RMSE": model_test_rmse}
+    # return {"train_RMSE": model_train_rmse, "test_RMSE": model_test_rmse}
+    pred_num = params.test_number
+    # 上限設定
+    if pred_num >= len(X_test):
+        pred_num = len(X_test) - 1
+    return {"predict": model_test_pred[pred_num][0], "answer": y_test.iloc[pred_num].values[0]}
